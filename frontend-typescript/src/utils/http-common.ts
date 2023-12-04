9dcdd0e70583569397aaeada2b7a5203af20f90b
StarsinLiver <141594965+StarsinLiver@users.noreply.github.com>
2023-12-04 11:17:59 +0900

    first
    enviroment source

--- frontend-typescript/src/utils/http-common.ts
+++ frontend-typescript/src/utils/http-common.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://43.201.18.3:8000/api",
+  baseURL: `${process.env.REACT_APP_BASE_URL}`,
   // baseURL: "http://localhost:8000/api",
   headers: {
     "Content-Type": "application/json",

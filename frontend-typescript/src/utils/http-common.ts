f14a58a83a80ea918b130a11d5d97d9667bd1017
devlife0409 <tkdghk2150@gmail.com>
2023-12-04 11:21:57 +0900

    12-04(1)
    홈페이지 쪼금 수정,
    finalPrice <-> 할인계산가격 맞춤

--- frontend-typescript/src/utils/http-common.ts
+++ frontend-typescript/src/utils/http-common.ts
@@ -1,9 +1,9 @@
-import axios from "axios";
+import axios from 'axios';
 
 export default axios.create({
-  baseURL: "http://43.201.18.3:8000/api",
-  // baseURL: "http://localhost:8000/api",
-  headers: {
-    "Content-Type": "application/json",
-  }
+    // baseURL: "http://43.201.18.3:8000/api",
+    baseURL: 'http://localhost:8000/api',
+    headers: {
+        'Content-Type': 'application/json',
+    },
 });
